1f0877420c6ef56dd8bd01ad91116d2584e3deb5
ix-xerri <matt.xerri@gmail.com>
2012-12-04 01:41:22 +0100

    Added JSON

--- public/js/views/twitter.js
+++ public/js/views/twitter.js
@@ -3,8 +3,9 @@ define([
  	'underscore',
 	'backbone',
 	'collections/twitter',
+	'views/tweet',
   	'text!templates/twitter.html'
-], function($, _, Backbone, TwitterCollection, TwitterTemplate){
+], function($, _, Backbone, TwitterCollection, TweetView, TwitterTemplate){
 	var TwitterView = Backbone.View.extend({
 		template: _.template(TwitterTemplate),
     	initialize: function () {
@@ -13,11 +14,7 @@ define([
       		this.twitterCollection.query = this.options.query || 'sports';
     	},
     	
-    	events: {
-    		'click li': 'onTweetClick'
-    	},
-    
-		render: function () {
+    	render: function () {
 	    	this.loadResults();
 	    	return this;
 	    },
@@ -28,18 +25,27 @@ define([
 	      	
 	      	this.twitterCollection.fetch({
 	        	success: function (tweets) {
-	          		$(that.el).append(that.template({title: title, tweets: tweets.models, _:_}));
+	        		$(that.el).append(that.template({title: title}));
+	        		_.each(tweets.models, function (tweet) {
+	        			var tweetView = new TweetView({model: tweet})
+	        			
+	        			tweetView.on('renderJson', that.displayJson);
+	        			
+       		            $('ul.tweets').append(tweetView.render().el);
+       		        }, this);
 	        	}
 	     	});      
-	    },
-	    
-	    onTweetClick: function(){
-	    	//alert('click');
 	    },
 	    
 	    displayTitle: function(){
 	    	var title = this.options.query
 	    	return title.charAt(0).toUpperCase() + title.slice(1);
+	    },
+	    
+	    displayJson: function(model){
+	    	var $json = $('.json');
+	    	$json.removeAttr('style');
+	    	$json.html(JSON.stringify(model.toJSON(), null, 4));
 	    }
 	});
   	
